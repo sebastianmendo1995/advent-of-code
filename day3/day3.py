2c16cb9e6b7cb2bb55cb67f1b7eae4c5f807444d
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2021-12-09 10:58:08 -0500

    day 3 correction

--- day3/day3.py
+++ day3/day3.py
@@ -1,39 +1,85 @@
-my_file = open("diagnostic.txt", "r")
-content = my_file.read()
-binaryNumbers = content.split("\n")
-my_file.close()
-
-totalOfNumbers = len(binaryNumbers)
-
-digits = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-for number in binaryNumbers:
-    arr = list(number)
-    i = 0
-    while i < len(arr):
-        digits[i] = digits[i] + int(arr[i])
-        i += 1
-
-print(digits)
-print(totalOfNumbers)
+with open("diagnostic.txt") as f:
+    data = [x for x in f.read().split()]
 
 gamma = ""
 epsilon = ""
 
-j = 0
-while j < len(digits):
-    if(digits[j] > totalOfNumbers/2):
-        gamma += "1"
-        epsilon += "0"
+for b in range(0, len(data[0])):
+    one = 0 
+    zero = 0
+
+    for c in range(0, len(data)):
+        if data[c][b] == '0':
+            zero += 1
+        else:
+            one += 1
+    
+    if zero > one:
+        gamma += '0'
+        epsilon += '1'
     else:
-        gamma += "0"
-        epsilon += "1"
-    j += 1
+        gamma += '1'
+        epsilon += '0'
 
-gammaDecimal = int(gamma, 2)
-epsilonDecimal = int(epsilon, 2)
+g = int(gamma, 2)
+e = int(epsilon, 2)
 
-print(gammaDecimal * epsilonDecimal)
+print(g, e)
 
 #part 2
+
+i = 0
+
+while len(data) > 1:
+    one = 0
+    zero = 0
+    ones = []
+    zeroes = []
+
+    for c in range(0, len(data)):
+        if data[c][i] == '0':
+            zero += 1
+            zeroes.append(data[c])
+        else:
+            one += 1
+            ones.append(data[c])
+
+    if zero > one:
+        data = zeroes
+    else:
+        data = ones
+
+    i += 1
+
+oxygen = int(data[0], 2)
+
+with open("diagnostic.txt") as f:
+    data = [x for x in f.read().split()]
+
+i = 0
+
+while len(data) > 1:
+    one = 0
+    zero = 0
+    ones = []
+    zeroes = []
+
+    for c in range(0, len(data)):
+        if data[c][i] == '0':
+            zero += 1
+            zeroes.append(data[c])
+        else:
+            one += 1
+            ones.append(data[c])
+
+    if one < zero:
+        data = ones
+    else:
+        data = zeroes
+
+    i += 1
+
+co = int(data[0], 2)
+
+print(oxygen * co)
 

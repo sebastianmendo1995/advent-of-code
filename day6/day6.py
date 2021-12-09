d541c7d7c3229f1bf8bc272e531013a8b39f7296
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2021-12-09 09:13:58 -0500

    day 6 problem

--- day6/day6.py
+++ day6/day6.py
@@ -4,18 +4,24 @@ puzzle = open("puzzle.txt", "r").read()
 exampleLanterns = [int(i) for i  in example.split(",")]
 puzzleLanterns = [int(i) for i  in puzzle.split(",")]
 
-
 def lanternfish (lanterns, days):
 
   amountLanterns = [0,0,0,0,0,0,0,0,0]
 
   for i in lanterns:
-    amountLanterns[i] += 1 
-  
+    amountLanterns[i] += 1
+
   i = 0
+
   while i < days:
+    firstElement = amountLanterns[:1]
+    del amountLanterns[0]
+    amountLanterns[6] += firstElement[0]
+    amountLanterns.append(firstElement[0])
     i+=1
 
-ans = lanternfish(exampleLanterns, 80, 0)
-ansPuzzle = lanternfish(puzzleLanterns, 80, 0)
+  return sum(amountLanterns)
+
+ans = lanternfish(exampleLanterns, 256)
+ansPuzzle = lanternfish(puzzleLanterns, 256)
 print(ans, ansPuzzle)
